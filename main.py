8a35b2870698b6a3b47caf05dfa47a980c18f03f
Athomisos <37215934+Athomisos@users.noreply.github.com>
2020-12-21 21:21:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import zirkia
 
-test = zirkia.Application("DemoIrKIAPY", "Demo ZirkiaPy", "Aubertin Emmanuel")
+test = zirkia.Application("DemoZirkiaPY", "Demo ZirkiaPy", "Aubertin Emmanuel")
 zirkia.Post_Request('https://httpbin.org/status/500', "")
 print (test.AppName)
